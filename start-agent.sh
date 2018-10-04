6e05db3c78e02cc5ddff00914f0a896dd91e58c8
Dmitry Berezovsky <corvis@users.noreply.github.com>
2018-10-04 18:25:29 +0300

    Removed override for agent work dir

--- start-agent.sh
+++ start-agent.sh
@@ -2,7 +2,6 @@
 echo "Updating property file..."
 
 sed -i "s|^serverUrl=.*$|serverUrl=$TEAMCITY_SERVER_URL|" /srv/teamcity-agent/conf/buildAgent.properties
-sed -i "s|^workDir=.*$|workDir=/home/app/work|" /srv/teamcity-agent/conf/buildAgent.properties
 sed -i "s|^ownPort=.*$|ownPort=$AGENT_PORT|" /srv/teamcity-agent/conf/buildAgent.properties
 sed -i "s|^name=.*$|name=$AGENT_NAME|" /srv/teamcity-agent/conf/buildAgent.properties
 
